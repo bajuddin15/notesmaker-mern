17db8513a251c0511e0d71627673164de790bec8
Bajuddin Khan <bajuddinkhan786@gmail.com>
2022-11-08 17:57:58 +0530

    update landing page

--- frontend/src/screens/LandingPage/LandingPage.js
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -20,7 +20,7 @@ function LandingPage({ history }) {
         <Row>
           <div className="intro-text">
             <div>
-              <h1 className="title">Welcome to Note Zipper</h1>
+              <h1 className="title">Welcome to Notes Maker</h1>
               <p className="subtitle">One Safe place for all your notes.</p>
             </div>
             <div className="buttonContainer">
