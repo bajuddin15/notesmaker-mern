377ec170c7b1a6f0853a88de620a173d4fb8a0ad
Bajuddin Khan <bajuddinkhan786@gmail.com>
2022-11-08 18:04:41 +0530

    update  copyright in footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
     >
       <Container>
         <Row>
-          <Col className="text-center py-3">Copyright &copy; Note Zipper</Col>
+          <Col className="text-center py-3">Copyright &copy; Notes Maker</Col>
         </Row>
       </Container>
     </footer>
